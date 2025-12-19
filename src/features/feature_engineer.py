3f3ef2829a2a0e3be01ad75a76dc5f3e64d1385f
Steven Glass <StevenGlass365@gmail.com>
2025-12-19 12:39:12 +0200

    Finalised Feature Engineering Pipeline

--- src/features/feature_engineer.py
+++ src/features/feature_engineer.py
@@ -8,28 +8,26 @@ def time_aggregate(df: pd.DataFrame):
     """
     A function that aggregates the data into unique timestamps
     """
-
-    df = df.sort_values("InvoiceDate")
-    df["date"]   = df["InvoiceDate"].dt.date
-    df["hour"]   = df["InvoiceDate"].dt.hour
-    df["day"]    = df["InvoiceDate"].dt.day
-    df["week"]   = df["InvoiceDate"].dt.isocalendar().week
-    df["month"]  = df["InvoiceDate"].dt.to_period("M")
-    df["weekday"]= df["InvoiceDate"].dt.day_name()
-
+    Invoice_Date = df["InvoiceDate"]
     df["Revenue"] = df['Price'] * df['Quantity']
-
+    df["Date"]   = df["InvoiceDate"].dt.date
     df = (
     df
-    .groupby("date")
+    .groupby("Date")
     .agg(
         revenue=("Revenue", "sum"),
         quantity=("Quantity", "sum"),
         customers=("Customer ID", "nunique")
     )
     .sort_index()
+    .reset_index() #Makes sure that Date is now a column again and not an index (Still in order)
     )
-
+    df["Date"] = pd.to_datetime(df["Date"])     #Convert back to datetime object
+    df["Day"]    = df["Date"].dt.day
+    df["Week"]   = df["Date"].dt.isocalendar().week
+    df["Month"]  = df["Date"].dt.to_period("M")
+    df["Weekday"]= df["Date"].dt.day_name()
+    df["Is_Weekend"] = df["Weekday"].isin(["Friday", "Saturday", "Sunday"])
 
     return df
 
